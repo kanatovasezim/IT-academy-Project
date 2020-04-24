e8c346f45389102d7e8c9418f277cc71f386431d
ERERkin <erkin1399@gmail.com>
2020-04-24 18:35:10 +0600

    Check

---
+++ src/main/java/it/academy/ItAcademyProjectApplication.java
@@ -0,0 +1,13 @@
+package it.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAcademyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItAcademyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/academy/ItAcademyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItAcademyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
